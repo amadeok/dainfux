d0ea13542a58de8f7530b1ee25c7e1009ed8ae06
amadeok <43666702+amadeok@users.noreply.github.com>
2021-06-10 11:00:11 +0200

    Add files via upload

--- DAIN/batch.py
+++ DAIN/batch.py
@@ -18,16 +18,25 @@ else:
     print("variable not found")
     sys.exit()
 
-intro_skip = file_list[1].split(" ")[1]
-ending_skip = file_list[2].split(" ")[1]
+intro_skip_ori = file_list[1].split(" ")[1]
+ending_skip_ori = file_list[2].split(" ")[1]
 
 for y in range(3):
     file_list.pop(0)
 
 for x in range(len(file_list)):
-    number = str(file_list[x]).zfill(pad)
+    intro_skip = intro_skip_ori
+    ending_skip = ending_skip_ori
+
+    if len(file_list[x].split(" ")) == 1:
+        number = str(file_list[x]).zfill(pad)
+    else:
+        number = str(file_list[x].split(" ")[0]).zfill(pad)
+        intro_skip = file_list[x].split(" ")[1]
+        ending_skip = file_list[x].split(" ")[2]
+        
     curr_file = f"{file_name[0]}{number}{file_name[1]}"
-    print("||| current file:", curr_file, "|||")
+    print("||| current file:", curr_file, "|||",  "intro_skip: ", intro_skip, "intro_skip: ",  ending_skip)
     command = f"python -W ignore colab_interpolate.py     --netName DAIN_slowmotion --time_step 0.5 --input_file '/content/drive/MyDrive/{curr_file}' --output_dir /content/drive/MyDrive/DAIN --enable_transcoder 0 --overwrite 0 --upscale_only 0  --selective_interpolation 1 --dual_instance 1   --waifu2x_scale 2 --waifu2x_model ../waifu2x-ncnn-vulkan/models/models-upconv_7_anime_style_art_rgb --ph_this_bad_th 500 --use_debug_parts 0 --debug_nb_parts 0 --intro_skip {intro_skip} --ending_skip {ending_skip}"
     print("current command:", command)
 
