b025ba3f369a091ef093c93d21803e8b8d644dbd
amadeok <43666702+amadeok@users.noreply.github.com>
2021-06-10 14:28:19 +0200

    Add files via upload

--- DAIN/batch.py
+++ DAIN/batch.py
@@ -1,43 +1,62 @@
 import os, sys, subprocess as sp
-file_list = []
-with open("list.txt", "r") as inp:
-    file_list = inp.read().split("\n")
 
-print(file_list)
-file_name = file_list[0]
-if '!X!X!X' in file_list[0]:
-    file_name = file_list[0].split('!X!X!X')
-    pad = 3
-elif '!X!X' in file_list[0]:
-    file_name = file_list[0].split('!X!X')
-    pad = 2
-elif '!X' in file_list[0]:
-    file_name = file_list[0].split('!X')
-    pad = 1
-else:
-    print("variable not found")
-    sys.exit()
 
-intro_skip_ori = file_list[1].split(" ")[1]
-ending_skip_ori = file_list[2].split(" ")[1]
+def read_file():
+    global file_list; global file_name; global intro_skip_ori; global ending_skip_ori; global time_step_ori; global pad
+    with open("list.txt", "r") as inp:
+        file_list = inp.read().split("\n")
 
-for y in range(3):
-    file_list.pop(0)
+    print(file_list)
+    file_name = file_list[0]
+    if '!X!X!X' in file_list[0]:
+        file_name = file_list[0].split('!X!X!X')
+        pad = 3
+    elif '!X!X' in file_list[0]:
+        file_name = file_list[0].split('!X!X')
+        pad = 2
+    elif '!X' in file_list[0]:
+        file_name = file_list[0].split('!X')
+        pad = 1
+    else:
+        print("variable not found")
+        sys.exit()
+
+    intro_skip_ori = file_list[1].split(" ")[1]
+    ending_skip_ori = file_list[2].split(" ")[1]
+    time_step_ori = file_list[3].split(" ")[1]
+
+    for y in range(4):
+        file_list.pop(0)
+    return file_list
+
+read_file()
 
 for x in range(len(file_list)):
+    read_file()
     intro_skip = intro_skip_ori
     ending_skip = ending_skip_ori
+    time_step = time_step_ori
 
-    if len(file_list[x].split(" ")) == 1:
+    len_ = len(file_list[x].split(" "))
+    if len_ == 1:
         number = str(file_list[x]).zfill(pad)
-    else:
+    elif len_ >= 3:
         number = str(file_list[x].split(" ")[0]).zfill(pad)
         intro_skip = file_list[x].split(" ")[1]
         ending_skip = file_list[x].split(" ")[2]
-        
+        if len_ == 4:     
+            time_step = file_list[x].split(" ")[3]
+
+
+    elif len_ == 2:
+        number = str(file_list[x].split(" ")[0]).zfill(pad)
+        time_step = file_list[x].split(" ")[1]
+
+
     curr_file = f"{file_name[0]}{number}{file_name[1]}"
-    print("||| current file:", curr_file, "|||",  "intro_skip: ", intro_skip, "intro_skip: ",  ending_skip)
-    command = f"python -W ignore colab_interpolate.py     --netName DAIN_slowmotion --time_step 0.5 --input_file '/content/drive/MyDrive/{curr_file}' --output_dir /content/drive/MyDrive/DAIN --enable_transcoder 0 --overwrite 0 --upscale_only 0  --selective_interpolation 1 --dual_instance 1   --waifu2x_scale 2 --waifu2x_model ../waifu2x-ncnn-vulkan/models/models-upconv_7_anime_style_art_rgb --ph_this_bad_th 500 --use_debug_parts 0 --debug_nb_parts 0 --intro_skip {intro_skip} --ending_skip {ending_skip}"
+    print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+    print("||| current file:", curr_file, "|||",  "intro_skip: ", intro_skip, "intro_skip: ",  ending_skip, "|||", "time_step:", time_step)
+    command = f"python -W ignore colab_interpolate.py     --netName DAIN_slowmotion --time_step {time_step} --input_file '/content/drive/MyDrive/{curr_file}' --output_dir /content/drive/MyDrive/DAIN --enable_transcoder 0 --overwrite 0 --upscale_only 0  --selective_interpolation 1 --dual_instance 1   --waifu2x_scale 2 --waifu2x_model ../waifu2x-ncnn-vulkan/models/models-upconv_7_anime_style_art_rgb --ph_this_bad_th 500 --use_debug_parts 0 --debug_nb_parts 0 --intro_skip {intro_skip} --ending_skip {ending_skip}"
     print("current command:", command)
 
     os.system(command)
